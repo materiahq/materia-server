aa186e48760333573cfe14b5a842d699bd498bd0
Arnaud Richard <arnaud@oauth.io>
2016-01-18 18:28:05 +0100

    fix tests, removeField & save changes when commit history

--- test/addons.spec.js
+++ test/addons.spec.js
@@ -21,7 +21,7 @@ describe('Addons', () => {
 		})
 	})
 
-	it('should load the basic auth addon', () => {
+	it('should load the basic auth addon', (done) => {
 		expect(app.addons.getLength()).to.equal(1)
 		expect(app.api.endpoints.length).to.equal(1)
 		app.start().then(() => {
@@ -30,6 +30,7 @@ describe('Addons', () => {
 				expect(response.statusCode).to.equal(200)
 			})
 			app.stop()
-		})
+			done()
+		}).catch(done)
 	})
 })
