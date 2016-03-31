bec51bf353a5a3a50f78735301908671b496a290
Arnaud Richard <arnaud@oauth.io>
2016-03-31 18:39:45 +0200

    Added materia addons search
    References #5
    
    This also adds materia addons update [package] and materia addons install as placeholders for upcoming changes

--- cli/cmd/addons.js
+++ cli/cmd/addons.js
@@ -2,42 +2,99 @@
 
 let App = require('../../lib/app')
 
+function pipe_proc(proc) {
+	new Promise((accept, reject) => {
+		let gotStderr = false
+		proc.on('error', (err) => { reject(err) })
+		proc.stdout.on('data', (data) => { process.stdout.write(data) })
+		proc.stderr.on('data', (data) => { process.stderr.write(data); gotStderr = true })
+		proc.on('close', (code) => {
+			// notice that we had some error text with gotStderr ?
+			accept(code)
+		})
+	})
+}
+
+function action_all() {
+	console.error('not implemented yet')
+}
+
+function action(proc, messages) {
+	pipe_proc(proc).then((code) => {
+		if (code == 0)
+			console.log(messages.success)
+		else {
+			console.error(messages.failed)
+			console.error(messages.code + code.toString().yellow)
+		}
+	}).catch((err) => {
+		if (err.stdout)
+			console.error(err.stdout)
+		if (err.stderr)
+			console.error(err.stderr)
+		console.error(err.stack)
+	})
+}
+
 module.exports = {
 	matches: (args, options) => {
-		return args[0] == 'addons'
+		if (args[0] == 'addons') {
+			if (["install", "update", "remove", "search"].indexOf(args[1]) != -1) {
+				if ( ! args[2] && (args[1] == "remove" || args[1] == "search"))
+					return 1
+				return true
+			}
+			return 1
+		}
+		return false
 	},
 
 	exec: (args, options) => {
 		let cwd = process.cwd()
 		let cmd = args[1]
-		let app = new App(cwd, options)
-		let proc
-		if (cmd == "install")
-			proc = app.addonsTools.install(args[2])
-		else
-			return Promise.reject(new Error("Unknown command: materia addons " + cmd))
-		new Promise((accept, reject) => {
-			let gotStderr = false
-			proc.on('error', (err) => { reject(err) })
-			proc.stdout.on('data', (data) => { process.stdout.write(data) })
-			proc.stderr.on('data', (data) => { process.stderr.write(data); gotStderr = true })
-			proc.on('close', (code) => {
-				// notice that we had some error text with gotStderr ?
-				accept(code)
+
+		if (cmd == "search") {
+			let AddonsTools = require('../../lib/runtimes/tools/addons')
+			let addonsTools = new AddonsTools()
+			addonsTools.search(args[2]).then((results) => {
+				if ( ! results.length)
+					return console.log('No addon found')
+				console.log("Search results:")
+				console.log("")
+				for (let result of results) {
+					console.log('\t' + result.name.cyan + '\t' + result.description)
+				}
+			}).catch((err) => {
+				console.error(err.message.red)
 			})
-		}).then((code) => {
-			if (code == 0)
-				console.log('Addon installed !')
-			else {
-				console.error('Failed to install addon.')
-				console.error('Addon install finished with return code: ' + code.toString().yellow)
+			return
+		}
+
+		let app = new App(cwd, options)
+		if (cmd == "install") {
+			if (args[2]) {
+				action(app.addonsTools.install(args[2]), {
+					success: 'Addon installed !',
+					failed: 'Failed to install addon.',
+					code: 'Addon install finished with return code: '
+				})
+			} else {
+				action_all()
 			}
-		}).catch((err) => {
-			if (err.stdout)
-				console.error(err.stdout)
-			if (err.stderr)
-				console.error(err.stderr)
-			console.error(err.stack)
-		})
+		}
+		else if (cmd == "update") {
+			if (args[2]) {
+				action(app.addonsTools.install(args[2]), {
+					success: 'Addon updated !',
+					failed: 'Failed to update addon.',
+					code: 'Addon update finished with return code: '
+				})
+			} else {
+				action_all()
+			}
+		}
+		else if (cmd == "remove") {
+			// ...
+		}
 	}
 }
