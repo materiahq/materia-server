fa51aaffc48c26e5ad3108ebd84c1aaaba425d23
Arnaud Richard <arnaud@oauth.io>
2016-08-25 20:38:02 +0200

    Updated dependencies, better error messages.

--- cli/cmd/start.js
+++ cli/cmd/start.js
@@ -15,14 +15,16 @@ module.exports = {
 		options['runtimes'] = options['runtimes'] || 'core'
 		console.log('Starting ' + 'materia'.yellow + ' in ' + cwd.green)
 		let app = new App(cwd, options)
-		app.load().then(() => {
-			app.start()
-		}, (err) => {
+		app.load().catch((e) => {
+			console.error('Could not load app:', e && e.message)
+		}).then(() => {
+			return app.start()
+		}).catch((err) => {
 			if (err.stdout)
 				console.error(err.stdout)
 			if (err.stderr)
 				console.error(err.stderr)
-			console.error(err.stack)
+			console.error('Could not start server:', err && err.message)
 		})
 	}
 }
