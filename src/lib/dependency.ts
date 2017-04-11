9bf84e8608b898a7609b805ac33d68fe1bec3bdb
Thibaud Arnault <thibaud.arnault@gmail.com>
2017-04-11 04:20:28 -0400

    remove console.log

--- src/lib/dependency.ts
+++ src/lib/dependency.ts
@@ -5,15 +5,11 @@ import * as which from 'which'
 export class Dependency {
 	static check(cmd:string): Promise<any> {
 		return new Promise((resolve, reject) => {
-			console.log(cmd, which);
 			which(cmd, (err, path) => {
-				console.log(err, path);
 				if (err) {
-					console.log('error', err)
 					return reject(err)
 				}
 				else {
-					console.log('resolve', path)
 					return resolve(path)
 				}
 			})
