a02050cabd3965b9ded579993061bcea2a65849f
Thibaud Arnault <thibaud.arnault@gmail.com>
2018-06-25 07:42:58 -0400

    (Query) Add \n in FindOne logs

--- src/lib/entities/queries/findOne.ts
+++ src/lib/entities/queries/findOne.ts
@@ -79,7 +79,7 @@ export class FindOneQuery extends Query {
 	run(params, options):Promise<any> {
 		this.entity.app.logger.log(`${chalk.bold('(Query)')} FindOne - Run ${chalk.bold(this.entity.name)}.${chalk.bold(this.id)}`)
 		this.entity.app.logger.log(` └── Parameters: ${JSON.stringify(params)}`)
-		this.entity.app.logger.log(` └── Options: ${JSON.stringify(options)}`)
+		this.entity.app.logger.log(` └── Options: ${JSON.stringify(options)}\n`)
 		let sequelizeOpts
 		try {
 			sequelizeOpts = this.constructSequelizeOpts(params, options)
