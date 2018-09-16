1e6ddcf3f73e9f6437871437b19b0283fceee7fa
Thibaud Arnault <thibaud.arnault@gmail.com>
2018-09-16 03:10:14 -0400

    (Addons) return 404 when addon pkg does not exist

--- src/api/controllers/addons.ctrl.ts
+++ src/api/controllers/addons.ctrl.ts
@@ -8,35 +8,49 @@ export class AddonsController {
 	setup(req, res) {
 		this.app.watcher.disable();
 		const pkg = this.getPkgFromRequest(req);
-
-		this.app.addons.setConfig(pkg, req.body).then(result => {
-			this.app.watcher.enable();
-			res.status(200).json(result)
-		}).catch(err => {
-			this.app.watcher.enable();
-			res.status(500).json(err)
-		});
+		if (! pkg || ! this.app.addons.get(pkg)) {
+			res.status(404).send();
+		} else {
+			this.app.addons.setConfig(pkg, req.body).then(result => {
+				this.app.watcher.enable();
+				res.status(200).json(result)
+			}).catch(err => {
+				this.app.watcher.enable();
+				res.status(500).json(err)
+			});
+		}
 	}
 
 	enable(req, res) {
 		const pkg = this.getPkgFromRequest(req);
-		this.app.addons.get(pkg).enable().then(() =>
-			res.status(200).send()
-		).catch(e => res.status(500).json(e));
+		if (! pkg || ! this.app.addons.get(pkg)) {
+			res.status(404).send();
+		} else {
+			this.app.addons.get(pkg).enable().then(() =>
+				res.status(200).send()
+			).catch(e => res.status(500).json(e));
+		}
 	}
 
 	disable(req, res) {
 		const pkg = this.getPkgFromRequest(req);
-		this.app.addons.get(pkg).disable().then(() =>
-			res.status(200).send()
-		).catch(e => res.status(500).json(e));
+		if (! pkg || ! this.app.addons.get(pkg)) {
+			res.status(404).send();
+		} else {
+			this.app.addons.get(pkg).disable().then(() =>
+				res.status(200).send()
+			).catch(e => res.status(500).json(e));
+		}
 	}
 
 	bundle(req, res) {
 		const pkg = this.getPkgFromRequest(req);
-		const bundle = fs.readFileSync(this.app.addons.get(pkg).getBundlePath());
-		this.app.logger.log(pkg, bundle)
-		res.status(200).send( bundle )
+		if (! pkg || ! this.app.addons.get(pkg)) {
+			res.status(404).send();
+		} else {
+			const bundle = fs.readFileSync(this.app.addons.get(pkg).getBundlePath());
+			res.status(200).send( bundle )
+		}
 	}
 	private getPkgFromRequest(req) {
 		return req.params.owner
