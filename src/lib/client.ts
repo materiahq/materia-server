2a0b3b4406bcf3b2f852fb5126a479d60772af44
GeoAstronaute <jeanmarc.royy@gmail.com>
2018-10-27 18:31:28 +0200

    (Lib/Client) Clean old comments

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -26,24 +26,6 @@ export class Client {
 	load() {
 		this.app.logger.log(` └─┬ Client `)
 		this.config = this.app.config.get<IClientBuild>(this.app.mode, ConfigType.CLIENT);
-		/*if (fs.existsSync(path.join(this.app.path, '.materia', 'client.json'))) {
-			this.config = JSON.parse(fs.readFileSync(path.join(this.app.path, '.materia', 'client.json'), 'utf-8'))
-		}
-		const packageJson = JSON.parse(fs.readFileSync(path.join(this.app.path, 'package.json'), 'utf-8'))
-		if (packageJson && packageJson.scripts) {
-			if (!this.config.scripts) {
-				this.config.scripts = {}
-			}
-			if (! this.config.scripts.build) {
-				this.config.scripts.build = packageJson.scripts.build ? "build" : null;
-			}
-			if (! this.config.scripts.watch) {
-				this.config.scripts.watch = packageJson.scripts.watch ? "watch" : null;
-			}
-			if (! this.config.scripts.prod) {
-				this.config.scripts.prod = packageJson.scripts.prod ? "prod" : null;
-			}
-		}*/
 		if ( ! this.config ) {
 			this.config = { build: false }
 			this.app.logger.log(` │ └── ${chalk.bold('No build scripts detected')}`)
@@ -125,57 +107,4 @@ export class Client {
 		}
 		return false
 	}
-
-	/*set(src, build, scripts, autoWatch) {
-		this.config.src = src
-		this.config.build = build
-		// UPDATE STATIC FOLDER AT RUNTIME
-		this.app.server.dynamicStatic.setPath(path.join(this.app.path, build));
-		if ( ! this.config.scripts ) {
-			this.config.scripts = {}
-		}
-		this.config.scripts.build = scripts.build
-		this.config.scripts.watch = scripts.watch
-		this.config.scripts.prod = scripts.prod
-
-		this.config.autoWatch = autoWatch
-		this.watching = false
-		return this.save()
-	}
-
-	save() {
-		if ( this.config ) {
-			if ( this.config.src == 'client' && this.config.dist == 'client' && ! this.hasOneScript() ) {
-				return Promise.resolve()
-			}
-
-			let res = {
-				src: this.config.src,
-				scripts: {}
-			} as IClientConfig
-
-			if (this.config.dist != this.config.src && this.config.dist) {
-				res.dist = this.config.dist
-			}
-
-			if (this.hasBuildScript(ScriptMode.BUILD)) {
-				res.scripts.build = this.config.scripts.build
-			}
-
-			if (this.hasBuildScript(ScriptMode.PROD)) {
-				res.scripts.prod = this.config.scripts.prod
-			}
-
-			if (this.hasBuildScript(ScriptMode.WATCH)) {
-				res.scripts.watch = this.config.scripts.watch
-			}
-
-			if (this.config.autoWatch) {
-				res.autoWatch = this.config.autoWatch
-			}
-
-			fs.writeFileSync(path.join(this.app.path, '.materia', 'client.json'), JSON.stringify(res, null, 2))
-		}
-		return Promise.resolve()
-	}*/
 }
