28efeff899ce0a402ede06c6ed28150c5e1a8d36
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-03-18 10:36:56 +0100

    (Lib/PackageManager) Feature: new PackageManager class + abstract npm/yarn usage

---
+++ src/lib/package-manager.ts
@@ -0,0 +1,98 @@
+import { readFile, existsSync } from 'fs';
+import * as which from 'which';
+import { join } from 'path';
+import { Npm } from './package-manager/npm';
+import { Yarn } from './package-manager/yarn';
+
+export class PackageManager {
+	nodeModuleManager: Npm | Yarn;
+	managerName: string;
+
+	constructor(private basepath: string) {
+		this.managerName = this.getPackageManagerName();
+		this.nodeModuleManager = this.managerName === 'yarn' ? new Yarn(this.basepath) : new Npm(this.basepath);
+	}
+
+	runScriptInBackground(scriptName, cwd?: string) {
+		if (this.managerName === 'yarn') {
+			return this.nodeModuleManager.execInBackground('run', [scriptName], cwd ? cwd : this.basepath);
+		} else {
+			return this.nodeModuleManager.execInBackground('run-script', [scriptName], cwd ? cwd : this.basepath);
+		}
+	}
+
+	runScript(scriptName: string, cwd?: string, stream?: (data: any, error?: boolean) => void) {
+		if (this.managerName === 'yarn') {
+			return this.nodeModuleManager.exec('run', [scriptName], cwd ? cwd : this.basepath, stream);
+		} else {
+			return this.nodeModuleManager.exec('run-script', [scriptName], cwd ? cwd : this.basepath, stream);
+		}
+	}
+
+	uninstall(packageName: string, stream?: (data: any, error?: boolean) => void) {
+		if (this.managerName === 'yarn') {
+			return this.nodeModuleManager.exec('remove', [packageName], this.basepath, stream);
+		} else {
+			return this.nodeModuleManager.exec('uninstall', [packageName, '--save'], this.basepath, stream);
+		}
+	}
+
+	upgrade(packageName: string, stream?: (data: any, error?: boolean) => void) {
+		if (this.managerName === 'yarn') {
+			return this.nodeModuleManager.exec('upgrade', [packageName], this.basepath, stream);
+		} else {
+			return this.nodeModuleManager.exec('upgrade', [packageName, '--save'], this.basepath, stream);
+		}
+	}
+
+	install(packageName: string, stream?: (data: any, error?: boolean) => void) {
+		if (this.managerName === 'yarn') {
+			return this.nodeModuleManager.exec('add', [packageName], this.basepath, stream);
+		} else {
+			return this.nodeModuleManager.exec('install', [packageName, '--save'], this.basepath, stream);
+		}
+	}
+
+	installAll(stream?: (data: any, error?: boolean) => void) {
+		if (this.managerName === 'yarn') {
+			return this.nodeModuleManager.exec('install', [], this.basepath, stream);
+		} else {
+			return this.nodeModuleManager.exec('install', [], this.basepath, stream);
+		}
+	}
+
+	getPackageManagerName(): string {
+		if (existsSync(join(this.basepath, 'yarn.lock')) && which.sync('yarn', { nothrow: true })) {
+			return 'yarn';
+		}
+		return 'npm';
+	}
+
+	getPackageJson(): Promise<any> {
+		return new Promise((resolve, reject) => {
+			const path = join(this.basepath, 'package.json');
+			readFile(path, 'utf-8', (err, data) => {
+				if (err) {
+					return reject(err);
+				}
+				resolve(JSON.parse(data));
+			});
+		});
+	}
+
+	getDependencies(): Promise<any> {
+		return this.getPackageJson().then(packageJson => packageJson.dependencies);
+	}
+
+	getDevdependencies(): Promise<any> {
+		return this.getPackageJson().then(packageJson => packageJson.devDependencies);
+	}
+
+	getScripts(): Promise<any> {
+		return this.getPackageJson().then(packageJson => packageJson.scripts);
+	}
+
+	hasNodeModules(): boolean {
+		return existsSync(join(this.basepath, 'node_modules'));
+	}
+}
---
+++ src/lib/package-manager/npm.ts
@@ -0,0 +1,67 @@
+import * as execa from 'execa';
+import * as which from 'which';
+
+export class Npm {
+
+	constructor(private cwd: string) {}
+
+	execInBackground(command: string, params?: string[], cwd?: string) {
+		return this.getNpmPath().then(npmPath => {
+			const npmProc = execa(npmPath, [command, ...params], {
+				cwd: cwd ? cwd : this.cwd
+			});
+			return { proc: npmProc };
+		});
+	}
+
+	exec(command: string, params?: string[], cwd?: string, stream?: (data: any, error?: boolean) => void): Promise<any> {
+		return new Promise((resolve, reject) => {
+			let data = '';
+			return this.getNpmPath().then(npmPath => {
+				if (stream) {
+					stream(`$ npm ${command} ${params.join(' ')}`);
+				}
+				const proc = execa(npmPath, [command, ...params], {
+					cwd: cwd ? cwd : this.cwd
+				});
+				let working = true;
+				proc.stdout.on('data', d => {
+					if (stream && working) {
+						stream(d.toString());
+					}
+					data += d;
+				});
+				proc.stderr.on('data', (d) => {
+					if (stream && working) {
+						stream(d.toString(), true);
+					}
+					data += d;
+				});
+
+				proc.on('close', (code) => {
+					working = false;
+					if (code == 0) {
+						return resolve(data);
+					} else {
+						return reject({
+							code,
+							data
+						});
+					}
+				});
+			});
+		});
+	}
+
+	private getNpmPath() {
+		return new Promise((resolve, reject) => {
+			which('npm', (err, npmPath) => {
+				if (err && ! npmPath) {
+					reject(err);
+				} else {
+					resolve(npmPath);
+				}
+			});
+		});
+	}
+}
