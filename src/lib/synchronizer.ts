a02bd9e6a8117a42d8f8401ec0f9ce7fe036b585
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-05-27 21:25:11 +0200

    (Lib/synchronizer) Fix compareField: modify fk default/defaultValue only (1)

--- src/lib/synchronizer.ts
+++ src/lib/synchronizer.ts
@@ -5,7 +5,6 @@ import { App } from './app';
 import { MigrationType } from './history';
 import { DBEntity } from './entities/db-entity';
 import { Entity } from './entities/entity';
-import { Database } from './database';
 
 export class Synchronizer {
 
@@ -60,7 +59,7 @@ export class Synchronizer {
 			field.defaultValue = 'now()';
 		}
 
-		if (Database.isSQLite && field.isRelation && field.isRelation.type === 'belongsTo') {
+		if (this.app.database && this.app.database.type === 'sqlite' && field.isRelation && field.isRelation.type === 'belongsTo') {
 			if ( ! field.onDelete || field.onDelete && field.onDelete.toUpperCase() === 'CASCADE') {
 				field.default = true;
 				field.defaultValue = 0;
