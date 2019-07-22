ced65aba29053c69fb4f6491c25c686262968fd3
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-07-22 12:25:50 +0200

    (Server) Feat: allow disabling CORS

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -51,13 +51,15 @@ export class Server {
 			this.expressApp.use(morgan('dev'));
 		}
 
-		// TODO: Option to enable / disable CORS API call
-		this.expressApp.use(function (req, res, next) {
-			res.header('Access-Control-Allow-Origin', '*');
-			res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-			res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-			next();
-		});
+		const conf = this.app.config.get<IServerConfig>(this.app.mode, ConfigType.SERVER);
+		if (conf.cors == null || conf.cors) {
+			this.expressApp.use(function (req, res, next) {
+				res.header('Access-Control-Allow-Origin', '*');
+				res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+				res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+				next();
+			});
+		}
 
 		this.expressApp.use(errorHandler());
 
