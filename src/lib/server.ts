7a0f15cd26683c956788cbe890015433eff85630
GeoAstronaute <jeanmarc.royy@gmail.com>
2019-07-22 12:26:26 +0200

    (Server) Fix: add DELETE method to CORS

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -55,7 +55,7 @@ export class Server {
 		if (conf.cors == null || conf.cors) {
 			this.expressApp.use(function (req, res, next) {
 				res.header('Access-Control-Allow-Origin', '*');
-				res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+				res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
 				res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 				next();
 			});
